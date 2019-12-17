5150779201d2615142167cd17fb8e45c180b3489
Fatih <nakay_f@hotmail.com>
2019-12-17 19:29:48 +0100

    initial commit

---
+++ app/src/main/java/com/example/mynotificationapp/NotificationHelper.java
@@ -0,0 +1,36 @@
+package com.example.mynotificationapp;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class NotificationHelper {
+
+
+    public static void displayNotification(Context context, String title, String body) {
+
+        Intent intent = new Intent(context, ProfileActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context,
+                100,
+                intent,
+                PendingIntent.FLAG_CANCEL_CURRENT
+        );
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context, MainActivity.CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_android_black_24dp)
+                        .setContentTitle(title)
+                        .setContentText(body)
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true)
+                        .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        NotificationManagerCompat notificationMgr = NotificationManagerCompat.from(context);
+        notificationMgr.notify(1, mBuilder.build());
+    }
+}
